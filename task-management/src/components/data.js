a0bf7ea11c0eeeb99164cc9f4e4d6c71e32f2dbc
edwardmartin1 <edwardmartin1@yahoo.com>
2025-11-01 16:51:02 -0500

    first commit

---
+++ task-management/src/components/data.js
@@ -0,0 +1,20 @@
+export const taskList = 
+[
+    {
+        id: 1,
+        title: "Clean Bathroom",
+        description: "Wash the floor.  Clean the sink and bathtub.",
+    },     
+    {
+        id: 2,
+        title: "Clean Kitchen",    
+        description: "Wash and dry the dishes.  Sweep the floor.  Clean out the refrigerator.",
+        
+    },     
+    {
+        id: 3,
+        title: "Wash Car",
+        description: "Wash the outside of car.  Wax the outside of car.  Wash the inside of car.",
+    },
+];
+
