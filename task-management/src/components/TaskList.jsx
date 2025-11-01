b2f0270803d168e135438034a67912ba565166c2
edwardmartin1 <edwardmartin1@yahoo.com>
2025-11-01 16:56:22 -0500

    final changes

--- task-management/src/components/TaskList.jsx
+++ task-management/src/components/TaskList.jsx
@@ -1,33 +1,24 @@
-
 import {Link} from "react-router";
 
 const TaskList = ({tasks}) =>
 {
-
     return (
         <div className="list">
             <h2>Task List</h2>
-            {/*<ul>*/}
 
-                {tasks.length === 0
-                    ? (<p>No tasks available.</p>)
-                    : (
-                        <ul>
-                            {tasks.map(task => 
-                            (
-                                <li key={task.id}>
-                                <Link to={`/taskdetail/${task.id}`}>{task.title}</Link>
-                
-                                {/*<h2>{task.title}</h2>*/}
-                                {/*<p>{task.description}</p>*/}
-                                </li>
-                            ))}
-                        </ul>
-                      )  
-                }
-
-                {/*}))*/}
-            {/*</ul>*/}
+            {tasks.length === 0
+                ? (<p>No tasks available.</p>)
+                : (
+                    <ul>
+                        {tasks.map(task => 
+                        (
+                            <li key={task.id}>
+                                <Link to={`/taskdetail/${task.id}`}>{task.title}</Link>               
+                            </li>
+                        ))}
+                    </ul>
+                  )  
+            }
         </div>
     );
 };
